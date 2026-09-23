dcc23586832b4b66639dfc72112629dedb9b7835
Infinitespacemechanic <roblaakkonen@gmail.com>
2026-09-22 21:13:19 -0400

    Add files via upload

---
+++ curvedfoot.lean
@@ -0,0 +1,9 @@
+def Clock := Fin 720
+def straightFoot : Nat := 12
+def straightFall : Float := 1.0
+def curvedFoot : Float := 1.0472 -- π/3 rad
+def geodesicFall : Float := 1.0472
+def persistentAsymmetry : Float := 0.0472
+def deviationPercent : Float := 4.72
+
+def theta : Float := 1.047197551 -- π/3 = 60°
